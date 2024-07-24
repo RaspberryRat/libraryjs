736a242ee52b844c5bf2f790cbec5673777e9ffa
Raspberry Rat <raspberryratman@gmail.com>
2024-07-23 18:26:07 -0600

    book constructor function added

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const myLibrary = [];
+
+function Book(title, author, pages, haveRead) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.haveRead = haveRead;
+
+};
+
+function addBookToLibrary() {
+  // take input add to library
+};
