639d7dcbb116b9f625e0cc724ca0370d358a3c8c
Raspberry Rat <raspberryratman@gmail.com>
2024-07-23 20:42:50 -0600

    can add new books to library and they will display on page

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const myLibrary = [];
+let libraryIndex = 0
 
 const addBookBtn = document.getElementById('add-book-btn')
 const newBookBtn = document.getElementById('new-book-btn')
@@ -12,10 +13,16 @@ function Book(title, author, pages, haveRead) {
   this.haveRead = haveRead;
 };
 
-function addBookToLibrary() {
-  // take input add to library
-  // variable to call book function
-  // take new varialbe and add to mylibrary constant
+function addBookToLibrary(event) {
+  let title = document.getElementById('title').value;
+  let author = document.getElementById('author').value;
+  let pages = document.getElementById('page-count').value;
+  let haveRead = document.getElementById('have-read').checked;
+  event.preventDefault();
+
+  let newBook = new Book(title, author, pages, haveRead);
+  myLibrary.push(newBook);
+  printBooks()
 };
 
 const book1 = new Book('The Hobbit', 'J.R.R Tolkien', 355, true)
@@ -24,29 +31,34 @@ const book2 = new Book('Spam Nation', 'Brian Krebs', 250, false)
 myLibrary.push(book1)
 myLibrary.push(book2)
 
-myLibrary.forEach((book) => {
-  let list = document.createElement('ul');
-  bookShelf.appendChild(list);
+function printBooks() {
+  for (; libraryIndex < myLibrary.length; libraryIndex++) {
+    let book = myLibrary[libraryIndex];
 
-  let title = document.createElement('li');
-  title.innerText = `Title: ${book.title}`;
-  list.appendChild(title);
+    let list = document.createElement('ul');
+    bookShelf.appendChild(list);
 
-  let author = document.createElement('li');
-  author.innerText = `Author: ${book.author}`;
-  list.appendChild(author);
+    let title = document.createElement('li');
+    title.innerText = `Title: ${book.title}`;
+    list.appendChild(title);
 
-  let pageCount = document.createElement('li');
-  pageCount.innerText = `${book.pages} pages`
-  list.appendChild(pageCount);
+    let author = document.createElement('li');
+    author.innerText = `Author: ${book.author}`;
+    list.appendChild(author);
 
-  let read = document.createElement('li');
-  read.innerHTML = `Read book? ${book.haveRead}`;
-  list.appendChild(read);
+    let pageCount = document.createElement('li');
+    pageCount.innerText = `${book.pages} pages`
+    list.appendChild(pageCount);
 
-
-});
-
+    let read = document.createElement('li');
+    read.innerHTML = `Read book? ${book.haveRead}`;
+    list.appendChild(read);
+  };
+};
 newBookBtn.addEventListener('click', () => {
   formContainer.classList.toggle('hidden')
 });
+
+addBookBtn.addEventListener('click', addBookToLibrary, false);
+
+printBooks();
